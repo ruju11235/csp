f8e6b9e37c7e90e8df7b5728337e2d35b6d85895
rujuta <rujuta@fls>
2024-10-09 21:46:06 -0700

    fix issue #7

--- src/digit_printer.js
+++ src/digit_printer.js
@@ -1,7 +1,27 @@
 // Implement https://github.com/ruju11235/csp/issues/7
 
-
-function print_digits()
+function print_list(ds)
 {
-    
+    var i = ds.length - 1;
+    while (i >= 0)
+    {
+        console.log(ds[i]);
+        i = i - 1;
+    }
 }
+
+function print_digits(a)
+{
+    var ds = [];
+    var i = 0;
+    while (a >= 10)
+    {
+        ds[i] = a % 10;
+        a = Math.floor(a / 10);
+        i = i + 1;
+    }
+    console.log(a);
+    print_list(ds);
+}
+
+print_digits(9);

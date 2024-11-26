238cf8a49236eaa0b794c811db5cfb4716d9fc0a
rujuta <rujuta@fls>
2024-11-26 15:10:37 -0800

    update palindromic string check program

--- src/palindromic_string_check.js
+++ src/palindromic_string_check.js
@@ -9,17 +9,33 @@ function reverse(s) {
         reversed = reversed + s[i];
         i = i - 1;
     }
-
     return reversed;
 }
 
 // Checks if a given string is a palindrome.
 // Returns true if the string is the same when reversed,
 // false otherwise.
-function is_palindrome(s)
+function is_palindrome_1(s)
 {
     return s === reverse(s);
 }
 
-var s = "redivider";
-console.log(is_palindrome(s));
+function is_palindrome_2(s)
+{
+    var i = 0
+    var n = s.length - 1;
+    while (i < n)
+    {
+        if (s[i] !== s[n])
+        {
+            return false;
+        }
+        i = i + 1;
+        n = n - 1;
+    }
+    return true;
+}
+
+var s = "weryykrew";
+console.log(is_palindrome_1(s));
+console.log(is_palindrome_2(s));

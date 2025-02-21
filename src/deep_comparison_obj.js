57854e21dd72b6da3ba2e2815ec83d4b73206194
ruju11235 <rujuta.mhaswade@gmail.com>
2025-02-20 16:01:50 -0800

    fix issue #17

---
+++ src/deep_comparison_obj.js
@@ -0,0 +1,37 @@
+/*
+
+*/
+
+// 
+function deepEqual(a, b)
+{
+    if (a === b)
+    {
+        return true;
+    }
+    if (a === null || b === null)
+    {
+        return false;
+    }
+    if (typeof a == "object" && typeof b == "object")
+    {
+        let keysa = Object.keys(a);
+        let keysb = Object.keys(b);
+        if (keysa.length !== keysb.length)
+        {
+            return false;
+        }
+        for (let i = 0; i < keysa.length; i++)
+        {
+            let inner_comparison = deepEqual(a[keysa[i]], b[keysa[i]]);
+            if (!inner_comparison)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+console.log(deepEqual(1, 2));
